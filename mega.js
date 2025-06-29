d575869c92b6cd698c07e91367319deaf86d95a2
WhiteDevilProgrammer99 <Denuwauk@gmail.com>
2025-06-29 08:55:26 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'vapis85474@konetas.com',
-    password: 'boxtermd1235',
+    email: 'gejic46124@chaladas.com',
+    password: 'White@123',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
